5bce3fdf4382caefcccbb25017e7398c1dfa7ac4
TuyaSotr <168427341+TuyaSotr@users.noreply.github.com>
2024-09-24 22:24:09 +0900

    auth_key.py

--- pages33/auth_key.py
+++ pages33/auth_key.py
@@ -1,21 +1,21 @@
 
 class AuthKey:
-    number = '79148206388'                          #Ваш номер
+    number = ''                          #Ваш номер
 
     # Регистрация
-    mail_regis = 'tuyara.sotrudnikova@gmail.com'    #<-Для регистрации надо написать почту
-    password_regis = 'Praktic33'                    #Пароль
-    confir_pass_reg = 'Praktic33'                   #Подтверждение пароля
+    mail_regis = ''           #<-Для регистрации надо написать почту
+    password_regis = ''                    #Пароль
+    confir_pass_reg = ''                   #Подтверждение пароля
 
 
     # Авторизация по логину и паролю
-    mail_auth = 'tuyara.sotrudnikova@gmail.com'     #Почта созданный(ого) и прикрепленный(ого) в учетном записе "Ростелеком"
-    login = 'rtkid_1727009583205'                   #Логин можно посмотреть в учетном записе "Ростелеком"
-    password_auth = 'Praktic33'                     #Пароль
+    mail_auth = ''         #Почта созданный(ого) и прикрепленный(ого) в учетном записе "Ростелеком"
+    login = ''                   #Логин можно посмотреть в учетном записе "Ростелеком"
+    password_auth = ''                     #Пароль
 
 
     #Восстановление пароля
-    new_password_recovery = 'Qwerty1234'            #Новый пароль для учетной записи
-    confir_pass_recovery = 'Qwerty1234'             #Подтверждение пароля
+    new_password_recovery = ''            #Новый пароль для учетной записи
+    confir_pass_recovery = ''             #Подтверждение пароля
 
 
