d5928a0fddee6f5ac111f0496e8c22e6a1b30d71
TuyaSotr <168427341+TuyaSotr@users.noreply.github.com>
2024-09-24 21:12:37 +0900

    Add files via upload

---
+++ pages33/auth_key.py
@@ -0,0 +1,21 @@
+
+class AuthKey:
+    number = '79148206388'                          #Ваш номер
+
+    # Регистрация
+    mail_regis = 'tuyara.sotrudnikova@gmail.com'    #<-Для регистрации надо написать почту
+    password_regis = 'Praktic33'                    #Пароль
+    confir_pass_reg = 'Praktic33'                   #Подтверждение пароля
+
+
+    # Авторизация по логину и паролю
+    mail_auth = 'tuyara.sotrudnikova@gmail.com'     #Почта созданный(ого) и прикрепленный(ого) в учетном записе "Ростелеком"
+    login = 'rtkid_1727009583205'                   #Логин можно посмотреть в учетном записе "Ростелеком"
+    password_auth = 'Praktic33'                     #Пароль
+
+
+    #Восстановление пароля
+    new_password_recovery = 'Qwerty1234'            #Новый пароль для учетной записи
+    confir_pass_recovery = 'Qwerty1234'             #Подтверждение пароля
+
+
